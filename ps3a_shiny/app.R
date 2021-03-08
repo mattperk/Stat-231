0e6a7317d8d5f6e76d4e717894e2c6486c88eff6
mattperk <matthewperkins248@gmail.com>
2021-03-08 13:55:28 -0500

    ps3a

---
+++ ps3a_shiny/app.R
@@ -0,0 +1,29 @@
+# app.R
+
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput(inputId = "num", 
+              label = "Choose a number", 
+              value = 25, min = 1, max = 100),
+  textInput(inputId = "title", 
+            label = "Write a title",
+            value = "Histogram of Random Normal Values"),
+  navlistPanel(
+    tabPanel(title = "Normal data",
+      plotOutput("hist")),
+    tabPanel(title = "Stats",
+      verbatimTextOutput("stats"))
+  )
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num), main = input$title)
+  })
+  output$stats <- renderPrint({
+    summary(rnorm(input$num))
+  })
+}
+
+shinyApp(ui = ui, server = server)
